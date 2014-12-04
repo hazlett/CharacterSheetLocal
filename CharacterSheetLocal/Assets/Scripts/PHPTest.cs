fbd1bd19d28424ddb8a3a508be866668ddbbb0f1
David Hazlett <hazlett.206@gmail.com>
2014-12-04 02:41:09 -0500

    character iteration

--- CharacterSheetLocal/Assets/Scripts/PHPTest.cs
+++ CharacterSheetLocal/Assets/Scripts/PHPTest.cs
@@ -3,54 +3,108 @@ using System.Collections;
 using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
+using System.Collections.Generic;
+using System;
 public class PHPTest : MonoBehaviour {
-    private string characterName = "";
+
+    private List<string> characterNames;
     void Start()
     {
-
+        Refresh();
         
     }
 
 
     void OnGUI()
     {
-        characterName = GUILayout.TextField(characterName);
-        if (GUILayout.Button("LOAD CHARACTER"))
+        if (GUILayout.Button("REFRESH"))
         {
-            string url = "http://hazlett206.ddns.net/DND/LoadCharacter.php";
-            WWWForm form = new WWWForm();
-            form.AddField("characterName", characterName);
-            WWW www = new WWW(url, form);
-
-            StartCoroutine(WaitForRequest(www));
+            Refresh();
         }
+        foreach(string name in characterNames)
+        {
+            if (GUILayout.Button(name))
+            {
+                LoadCharacter(name);
+            }
+        }
+    
+    }
+    private void Refresh()
+    {
+        characterNames = new List<string>();
+        string url = "http://hazlett206.ddns.net/DND/GetCharacters.php";
+        WWWForm form = new WWWForm();
+        WWW www = new WWW(url);
+
+        StartCoroutine(RefreshCharactersList(www));
     }
 
-    IEnumerator WaitForRequest(WWW www)
- {
-     yield return www;
-    
-     // check for errors
-     if (www.error == null)
-     {
-         Debug.Log("WWW Ok!: " + www.text);
-         XmlDocument doc = new XmlDocument();
-         doc.LoadXml(www.text);
+    private void LoadCharacter(string file)
+    {
+        string url = "http://hazlett206.ddns.net/DND/LoadCharacter.php";
+        WWWForm form = new WWWForm();
+        form.AddField("fileName", file);
+        WWW www = new WWW(url, form);
 
-         Character obj = new Character();
-         XmlSerializer serializer = new XmlSerializer(typeof(Character));
-         XmlReader reader = new XmlNodeReader(doc);
-             
-         obj = serializer.Deserialize(reader) as Character;
-         if (obj == null)
-         {
-             obj = new Character();
-         }
-         Debug.Log(obj.Name);
+        StartCoroutine(WaitForCharacter(www));
+    }
+    IEnumerator RefreshCharactersList(WWW www)
+    {
+        yield return www;
 
-         
-     } else {
-         Debug.Log("WWW Error: "+ www.error);
-     }    
- }  
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok!: " + www.text);
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(www.text);
+
+           
+            XmlSerializer serializer = new XmlSerializer(typeof(List<string>));
+            XmlReader reader = new XmlNodeReader(doc);
+
+            characterNames = serializer.Deserialize(reader) as List<string>;
+            if (characterNames == null)
+            {
+                Debug.Log("NULL NAMES");
+                characterNames = new List<string>();
+            }
+            Debug.Log(characterNames.Count);
+            Debug.Log("Character names loaded");
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }  
+    IEnumerator WaitForCharacter(WWW www)
+    {
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok!: " + www.text);
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(www.text);
+
+            Character obj = new Character();
+            XmlSerializer serializer = new XmlSerializer(typeof(Character));
+            XmlReader reader = new XmlNodeReader(doc);
+
+            obj = serializer.Deserialize(reader) as Character;
+            if (obj == null)
+            {
+                obj = new Character();
+            }
+            Debug.Log(obj.Name);
+
+
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
 }
